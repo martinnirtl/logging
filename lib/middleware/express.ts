6a868e7015f07b062cfba9e561af1de6f78d97a1
Martin Nirtl <martin@nirtl.rocks>
2020-11-02 09:42:05 +0100

    improve express middleware

--- lib/middleware/express.ts
+++ lib/middleware/express.ts
@@ -1,22 +1,95 @@
-import { RequestHandler } from "express";
-import logger, { getLogger } from "../core";
+import { NextFunction, Request } from "express";
+import { getLogger, Logger } from "../core";
 
-import { ILoggerOptions, Level } from "../core/types";
+import { ILoggerOptions, IMetadata, Level } from "../core/types";
 
-export interface IExpressMiddleware extends RequestHandler { }
+export interface IExtendedRequest extends Request {
+  logger: Logger
+}
+
+export interface IExpressMiddleware {
+  (req: IExtendedRequest, res: Response, next: NextFunction): void
+}
+
+interface LogIncomingConfig {
+  level?: Level
+  metadata?: {
+    body?: boolean | string[]
+    headers?: boolean | string[]
+    query?: boolean | string[]
+    params?: boolean | string[]
+  }
+}
 
 export interface ILoggerOptionsExpress extends ILoggerOptions {
   addToRequest: boolean
-  logIncoming: boolean | Level
+  logIncoming: boolean | Level | LogIncomingConfig
 }
 
-export const getMiddleware = (options: ILoggerOptionsExpress): IExpressMiddleware =>
-  (req: any, _res, next) => {
-    req.logger = getLogger(options)
+const processLogIncoming = (config: boolean | Level | LogIncomingConfig): [enabled: boolean, level: Level] => {
+  switch (typeof config) {
+    case 'boolean':
+      return [true, 'info']
+    case 'string':
+      return [true, config]
+    case 'object':
+      return [true, config.level || 'info']
 
-    if (options.logIncoming) {
-      logger[typeof options.logIncoming === 'boolean' ? 'info' : options.logIncoming](`[${req.method} ${req.path}] processing request...`, { headers: req.headers, body: req.body, query: req.query })
+    default:
+      return [false, 'info']
+  }
+}
+
+const getFields = (obj: Record<string, string>, { metadata }: LogIncomingConfig): Record<string, string> | undefined =>
+  metadata && metadata === true ? obj :
+    Object.entries(obj).filter(([key]) => (metadata as string[]).includes(key)).reduce((agg: Record<string, string>, [key, value], i) => {
+      if (i === 0) {
+        agg = {}
+      }
+
+      agg[key] = value
+
+      return agg
+    }, {})
+
+const getMetadata = (config: boolean | Level | LogIncomingConfig, req: Request): IMetadata => {
+  switch (typeof config) {
+    case 'boolean':
+      return {}
+
+    case 'string':
+      return {}
+
+    case 'object':
+      const metadata: IMetadata = {}
+
+      if (config.metadata) {
+        for (const key of Object.keys(config.metadata)) {
+          metadata[key] = getFields((req as any)[key], config)
+        }
+      }
+
+      return metadata
+
+    default:
+      return {}
+  }
+}
+
+export const getMiddleware = (options: ILoggerOptionsExpress): IExpressMiddleware => {
+  const [enabled, level] = processLogIncoming(options.logIncoming)
+
+  const logger = getLogger(options)
+
+  return (req, _res, next) => {
+    req.logger = logger
+
+    if (enabled) {
+      logger[level](`[${req.method} ${req.path}] processing request...`,
+        getMetadata(options.logIncoming, req),
+      )
     }
 
     next()
   }
+}
